0cdcdc2d066affacc19cc273258b316c3cf6e4cd
鲍洪江 <baobao025@vip.qq.com>
2023-03-13 17:53:40 +0800

    新的NSFW的库，蛮好用的。

--- nsfw_api.py
+++ nsfw_api.py
@@ -1,15 +1,6 @@
 import os
 import opennsfw2 as n2
 
-# Predict single image
-# print(predict.classify(model, 'N1.png'))
-# {'2.jpg': {'sexy': 4.3454722e-05, 'neutral': 0.00026579265, 'porn': 0.0007733492, 'hentai': 0.14751932, 'drawings': 0.85139805}}
-
-# Predict multiple images at once
-# predict.classify(model, ['/Users/bedapudi/Desktop/2.jpg', '/Users/bedapudi/Desktop/6.jpg'])
-# {'2.jpg': {'sexy': 4.3454795e-05, 'neutral': 0.00026579312, 'porn': 0.0007733498, 'hentai': 0.14751942, 'drawings': 0.8513979}, '6.jpg': {'drawings': 0.004214506, 'hentai': 0.013342537, 'neutral': 0.01834045, 'porn': 0.4431829, 'sexy': 0.5209196}}
-
-# Predict for all images in a directory
 def is_safe(img_path):
     nsfw_probability = n2.predict_image(img_path)
     if nsfw_probability > 0.4:
@@ -17,16 +8,30 @@ def is_safe(img_path):
     return True
 
 
+# import opennsfw2 as n2
+
+# # To get the NSFW probability of a single image.
+# image_path = "path/to/your/image.jpg"
+
+# nsfw_probability = n2.predict_image(image_path)
+
+# # To get the NSFW probabilities of a list of images.
+# # This is better than looping with `predict_image` as the model will only be instantiated once
+# # and batching is used during inference.
+# image_paths = [
+#   "path/to/your/image1.jpg",
+#   "path/to/your/image2.jpg",
+#   # ...
+# ]
+
+# nsfw_probabilities = n2.predict_images(image_paths)
 
 
-# for i in a.keys() :
-#      t = a[i]['hentai']*2 + a[i]['porn']*2 +a[i]['sexy']
-#      if t > 0.6:
-#         print(a[i]['hentai']*3)
-#         print(a[i]['porn']*3 )
-#         print(a[i]['sexy'])
-#         #先取i的文件名
-#         i = i.split('\\')[-1]
-#         #复制到t1文件夹下
-#         shutil.copyfile('./images/'+i, './images/t1/'+i)
-#         print(i, t)
+
+# import opennsfw2 as n2
+
+# # The video can be in any format supported by OpenCV.
+# video_path = "path/to/your/video.mp4"
+
+# # Return two lists giving the elapsed time in seconds and the NSFW probability of each frame.
+# elapsed_seconds, nsfw_probabilities = n2.predict_video_frames(video_path)
